e5cc3ba2807878f221ae921045fc5d660d33217b
Milica <milica@ubuntu.(none)>
2014-06-02 18:15:50 +0300

    Databases and some classes

--- SubjectTests.py
+++ SubjectTests.py
@@ -5,35 +5,35 @@ from Subject import *
 
 class SubjectTest(unittest.TestCase):
     def test_immutability_of_name(self):
-        math = Subject("Math")
+        math = Subject("Math", [5])
         self.assertRaises(AttributeError, math.__setattr__, 'name', "Art")
 
 
 class HardSubjectTest(unittest.TestCase):
     def test_immutability_of_name(self):
-        math = HardSubject("Math")
+        math = HardSubject("Math", [5])
         self.assertRaises(AttributeError, math.__setattr__, 'name', "Art")
 
     def test_immutability_of_coefficient(self):
-        math = HardSubject("Math")
+        math = HardSubject("Math", [5])
         self.assertRaises(AttributeError, math.__setattr__, 'coefficient', 5)
 
 class MediumSubjectTest(unittest.TestCase):
     def test_immutability_of_name(self):
-        history = MediumSubject("History")
+        history = MediumSubject("History", [5])
         self.assertRaises(AttributeError, history.__setattr__, 'name', "Art")
 
     def test_immutability_of_coefficient(self):
-        history = HardSubject("History")
+        history = HardSubject("History", [5])
         self.assertRaises(AttributeError, history.__setattr__, 'coefficient', 1)
 
 class EasySubjectTest(unittest.TestCase):
     def test_immutability_of_name(self):
-        sport = MediumSubject("Sport")
+        sport = MediumSubject("Sport", [5])
         self.assertRaises(AttributeError, sport.__setattr__, 'name', "Art")
 
     def test_immutability_of_coefficient(self):
-        sport = HardSubject("Sport")
+        sport = HardSubject("Sport", [5])
         self.assertRaises(AttributeError, sport.__setattr__, 'coefficient', 1)
 
 
