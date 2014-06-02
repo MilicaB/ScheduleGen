6ebb4b79a9df1ae76da3912b3fe2fc13ea429dfd
Milica <milica@ubuntu.(none)>
2014-06-02 18:08:27 +0300

    Subject class

--- Subject.py
+++ Subject.py
@@ -1,11 +1,21 @@
 class Subject:
-    def __init__(self, name):
+    def __init__(self, name, workloads):
         self._name = name
+        self._workloads = workload
 
     @property
     def name(self):
         return self._name
 
+    @property
+    def workloads(self):
+        return self._workloads
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return str(self)
 
 class HardSubject(Subject):
     _coefficient = 3
